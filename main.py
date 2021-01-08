d14f388287f316eaf406c9dfd99ac38984cc07a2
ExtremelySunnyYk <chiayongkang@hotmail.com>
2021-01-08 19:28:54 +0800

    flask

--- main.py
+++ main.py
@@ -4,12 +4,34 @@ import requests
 import os
 import io
 
+import logging
+from decouple import config
+
+from telegram import Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+
+
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
+from telegram.ext import Updater, CommandHandler, Filters, CallbackContext
+
+# Enable logging
+from telegram.utils import helpers
+
 app = Flask(__name__)
 
-@app.route('/',methods = ["GET","POST"])
+TELEGRAM_URL = "https://api.telegram.org/bot1582533456:AAFswg2spaHuwD0x6O3pG3ajSx4wjBuQL4s/"
+
+@app.route('/shake',methods = ["GET","POST"])
 def get_shake():
     ''' Receives POST Request from App and calls the shake leg function on telegram.    '''
-    pass
+    user_name = request.data
+    requests.post(user_name,TELEGRAM_URL)
 
 if __name__ == '__main__':
    app.run()
--- test3.py
+++ test3.py
@@ -31,23 +31,15 @@ message_to_user = "Add this to groups that you want to notify about shaking legs
 humialiting_message = "I AM A PERSON WHO HAS NO SELF CONTROL"
 user_groups = {}
 
-# Define constants that will allow us to reuse the deep-linking parameters.
-CHECK_THIS_OUT = 'check-this-out'
-USING_ENTITIES = 'using-entities-here'
-SO_COOL = 'so-cool'
-
-
 def start(update,context):
     """Sends humiliating message to chats"""
     bot = context.bot
     user = update.message.from_user
     user_groups[user['username']] = update.message.chat_id
-    url = helpers.create_deep_linked_url(bot.get_me().username, CHECK_THIS_OUT, group=True)
+    url = helpers.create_deep_linked_url(bot.get_me().username, message_to_user, group=True)
     text = f"Add this to people you care about \n {url}"
 
-    # update.message.reply_text(f'Howdy Good day {update.effective_chat.first_name}')
     update.message.reply_text(text)
-    # send_humiliation()
 
 
 def humiliate(update,context):
